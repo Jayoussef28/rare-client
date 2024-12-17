581fe4a8aad148fe92e52135ea5973a1eae1d750
asparadise33 <abby.paradise@gmail.com>
2024-12-16 21:23:31 -0600

    added edit button

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 import { Button, Card } from 'react-bootstrap';
 import { deletePost } from '../api/PostData';
 
@@ -17,6 +18,9 @@ function PostCard({ postObj }) {
         <Card.Img variant="top" src={postObj.image_url} alt={postObj.title} style={{ height: '400px', borderRadius: '0.5rem' }} />
         <p className="card-text bold">{postObj.publication_date}</p>
         <p className="card-text bold">{postObj.content}</p>
+        <Link href={`/posts/edit/${postObj.id}`} passHref>
+          <Button variant="outline-dark" color="success">EDIT</Button>
+        </Link>
         <Button variant="outline-danger" onClick={deleteThisPost} className="m-2">
           DELETE
         </Button>
